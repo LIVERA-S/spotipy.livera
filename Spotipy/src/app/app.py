762719e0ed8ed9820b701d1b5bf7dc8f8f5ffb9c
Sasmitha Livera <livera.sasmitha@iisgalvanimi.edu.it>
2022-10-26 12:00:08 +0000

    try

---
+++ Spotipy/src/app/app.py
@@ -0,0 +1,14 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = 'c0b1eb0b7a7848eb8436567d5871b8c2', client_secret = '33394ae9bebc434b8ad0ff3acfb11640'))
+
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album)
